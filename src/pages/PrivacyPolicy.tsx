c397ce301ac9c79ee0f5ac62402ed150f077f6ff
Carlos Denner <carlosdenner@gmail.com>
2025-06-30 16:18:48 -0400

    chore: apply customization colors.primary_theme

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -101,7 +101,7 @@ const PrivacyPolicy = () => {
                       <strong>Website created by:</strong>{' '}
                       <a 
                         href="mailto:info@datasciencetech.ca" 
-                        className="text-blue-600 hover:text-blue-800"
+                        className="text-pink-600 hover:text-pink-800"
                       >
                         Data Science Tech
                       </a>
